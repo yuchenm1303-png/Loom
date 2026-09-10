74f5e09eb9dbda0359cbfd5a8a40634f6cb7ce6b
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-10 17:32:26 +0800

    fix(app-server): install title backfill hook

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 """Loom core application package."""
 
 from .thread_title_override import install as _install_thread_title_override
+from .thread_title_backfill import install as _install_thread_title_backfill
 
 _install_thread_title_override()
-del _install_thread_title_override
+_install_thread_title_backfill()
+del _install_thread_title_override, _install_thread_title_backfill
