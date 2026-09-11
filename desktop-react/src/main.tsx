2a56497ece45b73bcb747f22029ed517c8d966d6
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-11 10:14:03 +0800

    Wire refined header mark styling

--- desktop-react/src/main.tsx
+++ desktop-react/src/main.tsx
@@ -16,6 +16,7 @@ import "./components/settings-icon-alignment.css";
 import "./components/computer-use-hud.css";
 import "./components/inspector-tabs.css";
 import "./components/semantic-colors.css";
+import "./components/thread-header-mark-refinement.css";
 
 try {
   const desktop = JSON.parse(window.localStorage.getItem("loom.settings.desktop.v2") || "{}");
