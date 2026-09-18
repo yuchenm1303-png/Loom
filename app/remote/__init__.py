79613663faf77beaa78cba3218123235c2d03ab3
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-18 13:47:10 +0800

    refactor(remote): export channel-neutral remote stack

--- app/remote/__init__.py
+++ app/remote/__init__.py
@@ -1,18 +1,32 @@
-from .bridge import WeChatRemoteBridge
-from .state import WeChatRemoteBinding, WeChatRemoteStateStore
-from .wechat_customer_service import (
-    WeChatCustomerServiceClient,
-    WeChatCustomerServiceConfig,
-    WeChatCustomerServiceError,
-    WeChatInboundMessage,
+from .base import RemoteChannel, RemoteMessage, RemoteSessionState, sanitize_remote_text
+from .service import LoomRemoteService
+from .channels.weixin import (
+    DEFAULT_ILINK_BASE_URL,
+    WeixinApiClient,
+    WeixinApiError,
+    WeixinAuthenticationExpired,
+    WeixinChannel,
+    WeixinCredentials,
+    WeixinCredentialStore,
+    WeixinMonitor,
+    WeixinQrAuthenticator,
+    WeixinRemoteStateStore,
 )
 
 __all__ = [
-    "WeChatCustomerServiceClient",
-    "WeChatCustomerServiceConfig",
-    "WeChatCustomerServiceError",
-    "WeChatInboundMessage",
-    "WeChatRemoteBinding",
-    "WeChatRemoteBridge",
-    "WeChatRemoteStateStore",
+    "DEFAULT_ILINK_BASE_URL",
+    "LoomRemoteService",
+    "RemoteChannel",
+    "RemoteMessage",
+    "RemoteSessionState",
+    "WeixinApiClient",
+    "WeixinApiError",
+    "WeixinAuthenticationExpired",
+    "WeixinChannel",
+    "WeixinCredentials",
+    "WeixinCredentialStore",
+    "WeixinMonitor",
+    "WeixinQrAuthenticator",
+    "WeixinRemoteStateStore",
+    "sanitize_remote_text",
 ]
