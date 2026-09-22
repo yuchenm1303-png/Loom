ddee9e1be81f362dd766e3d7ab2115ca5eed59ac
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-22 05:32:10 -0700

    load inline thinking presentation styles

--- desktop-react/src/main.tsx
+++ desktop-react/src/main.tsx
@@ -9,6 +9,7 @@ import "./styles.css";
 import "./shell-fix.css";
 import "./components/model-panel-overrides.css";
 import "./components/reasoning-control.css";
+import "./components/inline-thinking.css";
 import "./components/inline-thinking-orb.css";
 import "./components/composer-stability.css";
 import "./components/composer-attachment-polish.css";
