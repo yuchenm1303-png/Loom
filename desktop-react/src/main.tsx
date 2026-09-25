1d9ca192a5c1016446ff283184bab6206c7ab380
yuchenm1303-png <yuchenm1303@gmail.com>
2026-09-24 20:31:46 -0700

    ui: load starter card polish

--- desktop-react/src/main.tsx
+++ desktop-react/src/main.tsx
@@ -33,6 +33,7 @@ import "./components/composer-control-pills.css";
 import "./components/project-details-panel-theme.css";
 import "./global-motion.css";
 import "./components/generation-motion.css";
+import "./components/starter-cards-polish.css";
 
 const CONVERSATION_WIDTHS: Record<string, string> = {
   focused: "740px",
